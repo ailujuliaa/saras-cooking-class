297a422e07b702d44c08b26fc3d9c5097d7f897c
ailujuliaa <juliamoumoraes@gmail.com>
2026-07-01 16:32:00 -0300

    teste

---
+++ projeto.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    string name;
+    cout << "1- Criar arquivo\n" 
+        << "2- Ler arquivo\n" 
+        << "3- Atualizar arquivo\n" 
+        << "4- Procurar arquivo\n" 
+        << "5- Excluir arquivo\n" 
+        << "6- Sair" << endl;
+        
+   
+    return 0;
+}
