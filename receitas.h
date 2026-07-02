772f2afc230341b0764cc175716d4bd87fd1df02
ailujuliaa <juliamoumoraes@gmail.com>
2026-07-02 01:04:08 -0300

    declarei umas funçoes no arquivo receitas.h e chamei elas no projeto.cpp, mas ainda não fiz a implementação das funções.

---
+++ receitas.h
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+void criar (){
+
+}
+
+void ler (){
+
+}
+
+void atualizar (){
+
+}
+
+void procurar (){
+
+}
+
+void excluir (){
+
+}
